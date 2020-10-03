7f5c0191812c5a5012d88bbf201a37d094f1e223
mertefe95 <61557140+mertefe95@users.noreply.github.com>
2020-10-03 13:55:16 +0300

    First commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,32 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import "../src/assets/tailwind.css";
+import SwellLogo from "../src/assets/images/swell-logo.png";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="body h-screen">
+      
+      <div className="header p-13">
+        <img className="p-14"  src={SwellLogo}/>
+      </div>
+
+      <div className="main flex justify-center items-center">
+        <div className="upper-main-content">
+          <p className="text-4xl font-medium">Verify your email address</p>
+          <p className="upper-second-p text-base">Please check your inbox for jaequery@gmail.com and enter the code below</p>
+        </div>
+        <div className="form-container">
+          <form className="form">
+            <input type="text"></input>
+            <button className="btn py-4 px-4 text-base border-solid border border-transparent cursor-pointer shadow-none rounded">Continue</button>
+          </form>
+        </div>
+
+        <div className="footnotes text-sm">
+          <p>Didn't receive it?</p>
+          <p className="foot-second-p">Send again</p>
+        </div>
+      </div>
     </div>
   );
 }
